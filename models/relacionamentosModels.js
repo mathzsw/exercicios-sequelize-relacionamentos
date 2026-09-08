becabbb90d0e01a00559e65a72bfdd1bc62154cc
Matheus Augusto Rodrigues de Alencar <matheus06alencar@gmail.com>
2026-09-08 18:55:09 -0300

    feat: adiciona relacionamento 1:N Autor e Livro

--- models/relacionamentosModels.js
+++ models/relacionamentosModels.js
@@ -1,5 +1,7 @@
 const Pessoa = require('./Pessoa');
 const Passaporte = require('./Passaporte');
+const Autor = require('./Autor');
+const Livro = require('./Livro');
 
 Pessoa.hasOne(Passaporte, {
   foreignKey: 'pessoaId',
@@ -9,4 +11,14 @@ Pessoa.hasOne(Passaporte, {
 Passaporte.belongsTo(Pessoa, {
   foreignKey: 'pessoaId',
   as: 'pessoa'
+});
+
+Autor.hasMany(Livro, {
+  foreignKey: 'autorId',
+  as: 'livros'
+});
+
+Livro.belongsTo(Autor, {
+  foreignKey: 'autorId',
+  as: 'autor'
 });
