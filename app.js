0cdc2e1e87d4eacad228645ba7c223109f236f31
Matheus Augusto Rodrigues de Alencar <matheus06alencar@gmail.com>
2026-09-08 18:52:12 -0300

    Implementa Exercício 2 e 3

--- app.js
+++ app.js
@@ -8,6 +8,43 @@ require('./models/relacionamentosModels');
 const app = express();
 const PORT = 3000;
 
+// Exercício 2 - cria uma Pessoa e seu Passaporte usando o método mágico
+app.get('/exercicio2', async (req, res) => {
+  try {
+    const pessoa = await Pessoa.create({
+      nome: 'Matheus'
+    });
+
+    await pessoa.createPassaporte({
+      numero: 'BR123456',
+      validade: '2030-12-31'
+    });
+
+    res.send('Pessoa e Passaporte criados com sucesso!');
+  } catch (erro) {
+    console.error(erro);
+    res.status(500).send('Erro ao criar Pessoa e Passaporte.');
+  }
+});
+
+// Exercício 3 - busca uma Pessoa pelo ID trazendo seu Passaporte
+app.get('/exercicio3', async (req, res) => {
+  try {
+    const pessoa = await Pessoa.findByPk(1, {
+      include: {
+        model: Passaporte,
+        as: 'passaporte'
+      }
+    });
+
+    console.log(pessoa ? pessoa.toJSON() : 'Pessoa não encontrada.');
+    res.send('Consulta realizada. Veja o terminal.');
+  } catch (erro) {
+    console.error(erro);
+    res.status(500).send('Erro ao consultar Pessoa.');
+  }
+});
+
 sequelize.sync()
   .then(() => {
     app.listen(PORT, () => {
